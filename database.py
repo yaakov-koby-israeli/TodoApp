ac1982ecd32848a7695253143e2464f6b2a8fdba
koby <kobyisr1@gmail.com>
2024-10-21 15:59:54 +0300

    Changing prod db to Render

--- database.py
+++ database.py
@@ -2,10 +2,17 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# render db
+SQLALCHEMY_DATABASE_URL = 'postgresql://todoapp_database_ntya_user:Pyp3WxpPluK0x6FQ89B5hbjQ6vp1BkY4@dpg-csb4onpu0jms73e35eug-a.frankfurt-postgres.render.com/todoapp_database_ntya'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+
 
 # IF USING SQLITE3
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 # IF USING POSTGRESQL
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/TodoAplicationDatabase'
